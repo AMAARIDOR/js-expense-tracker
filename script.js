f70da5f5989ebd4922b0bbfa510f9b246916adce
HZZRD <arish2k3@gmail.com>
2022-12-23 14:14:30 +0530

    optimize code with objects

--- script.js
+++ script.js
@@ -11,29 +11,28 @@ let deleteTableRow = (k) => {
 document.getElementById("addExpense").addEventListener("click", () => {
   let table = document.getElementById("expenseTable");
   let tableRow = table.insertRow(-1);
+
   let typeOfExpenseDataCell = tableRow.insertCell(-1);
   let nameOfExpenseDataCell = tableRow.insertCell(-1);
   let dateOfExpenseDataCell = tableRow.insertCell(-1);
   let amountOfExpenseDataCell = tableRow.insertCell(-1);
   let deleteButtonTableRow = tableRow.insertCell(-1);
 
-  let typeOfExpenseElementValue =
-    document.getElementById("typeOfExpense").value;
-  let nameOfExpenseElementValue =
-    document.getElementById("nameOfExpense").value;
-  let dateOfExpenseElementValue =
-    document.getElementById("dateOfExpense").value;
-  let amountOfExpenseElementValue =
-    document.getElementById("amountOfExpense").value;
+  let expenseValues = {
+    typeOfExpense: document.getElementById("typeOfExpense").value,
+    nameOfExpense: document.getElementById("nameOfExpense").value,
+    dateOfExpense: document.getElementById("dateOfExpense").value,
+    amountOfExpense: document.getElementById("amountOfExpense").value,
+  };
 
   i++;
 
   tableRow.id = `${i}`;
 
-  typeOfExpenseDataCell.textContent = typeOfExpenseElementValue;
-  nameOfExpenseDataCell.textContent = nameOfExpenseElementValue;
-  dateOfExpenseDataCell.textContent = dateOfExpenseElementValue;
-  amountOfExpenseDataCell.textContent = amountOfExpenseElementValue;
+  typeOfExpenseDataCell.textContent = expenseValues.typeOfExpense;
+  nameOfExpenseDataCell.textContent = expenseValues.nameOfExpense;
+  dateOfExpenseDataCell.textContent = expenseValues.dateOfExpense;
+  amountOfExpenseDataCell.textContent = expenseValues.amountOfExpense;
 
   deleteButtonTableRow.innerHTML = `<button onclick="deleteTableRow(this.parentNode.parentNode.rowIndex)" class="deleteButtons" id="deleteButtonTableRow${i}">X</button>`;
 });
